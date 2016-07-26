0ab6b787d95eeedc3877369cc131bc87b78e7802
Joaquim Rocha <jrocha.github@gmailbox.org>
2016-07-26 12:06:15 +0100

    Update brick_event.erl

--- src/brick_event.erl
+++ src/brick_event.erl
@@ -18,6 +18,8 @@
 
 -include("brick_event.hrl").
 
+-define(HANDLER(Type, Susbcriber), {?MODULE, {Type, Susbcriber}}).
+
 -behaviour(gen_event).
 
 -export([init/1, handle_event/2, handle_call/2, handle_info/2, terminate/2, code_change/3]).
@@ -30,11 +32,11 @@
 start_link() ->
 	gen_event:start_link({local, ?MODULE}).
 
-subscribe(Type, Susbcriber) ->
-	gen_event:add_handler(?MODULE, {?MODULE, {Type, Susbcriber}}, [Type, Susbcriber]).
+subscribe(Type, Subscriber) ->
+	gen_event:add_handler(?MODULE, ?HANDLER(Type, Subscriber), [Type, Subscriber]).
 
-unsubscribe(Type, Susbcriber) ->
-	gen_event:delete_handler(?MODULE, {?MODULE, {Type, Susbcriber}}, []).
+unsubscribe(Type, Subscriber) ->
+	gen_event:delete_handler(?MODULE, ?HANDLER(Type, Subscriber), []).
 
 event(Type, Name, Value) ->
 	Event = #brick_event{name=Name, value=Value},
@@ -46,36 +48,36 @@ event(Type, Name, Value) ->
 -record(state, {type, subscriber, ref}).
 
 %% init/1
-init([Type, Susbcriber]) ->
-	MonitorRef = erlang:monitor(process, Susbcriber),
-    {ok, #state{type=Type, subscriber=Susbcriber, ref=MonitorRef}}.
+init([Type, Subscriber]) ->
+	MonitorRef = erlang:monitor(process, Subscriber),
+	{ok, #state{type=Type, subscriber=Subscriber, ref=MonitorRef}}.
 
 %% handle_event/2
-handle_event({Type, Event}, State=#state{type=Type, subscriber=Susbcriber}) ->
-	Susbcriber ! Event,
-    {ok, State};
+handle_event({Type, Event}, State=#state{type=Type, subscriber=Subscriber}) ->
+	Subscriber ! Event,
+	{ok, State};
 handle_event(_Event, State) ->
 	{ok, State}.
 
 %% handle_call/2
 handle_call(_Request, State) ->
-    Reply = ok,
-    {ok, Reply, State}.
+	Reply = ok,
+	{ok, Reply, State}.
 
 %% handle_info/2
 handle_info({'DOWN', MonitorRef, _Type, _Object, _Info}, #state{ref=MonitorRef}) ->
-    remove_handler;
+	remove_handler;
 handle_info(_Info, State) ->
-    {ok, State}.
+	{ok, State}.
 
 %% terminate/2
 terminate(_Arg, #state{ref=MonitorRef}) ->
 	erlang:demonitor(MonitorRef),
-    ok.
+	ok.
 
 %% code_change/3
 code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
+	{ok, State}.
 
 %% ====================================================================
 %% Internal functions
