b1a5392d86421837be586f24e16889aece2acd0b
Joaquim Rocha <jrocha.github@gmailbox.org>
2016-07-26 11:47:22 +0100

    Create brick_config.erl

---
+++ src/brick_config.erl
@@ -0,0 +1,38 @@
+%%
+%% Copyright 2016 Joaquim Rocha <jrocha@gmailbox.org>
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%   http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%
+
+-module(brick_config).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+-export([get_env/1, get_env/2]).
+
+get_env(Param) -> 
+	{ok, Value} = application:get_env(brick, Param),
+	Value.
+
+get_env(Param1, Param2) ->
+	Config = get_env(Param1),
+	get_value(Param2, Config).
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+get_value(Param, Config) ->
+	{_, Value} = lists:keyfind(Param, 1, Config),
+	Value;
