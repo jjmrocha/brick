8650e70af7cc5f6b870fba6b4d620e3afa1b6863
Joaquim Rocha <jrocha.github@gmailbox.org>
2016-07-28 16:26:10 +0100

    Update brick_sup.erl

--- src/brick_sup.erl
+++ src/brick_sup.erl
@@ -38,11 +38,12 @@ init([]) ->
 	Clock = #{id => brick_hlc, start => {brick_hlc, start_link, []}, restart => permanent, type => worker},
 	Cluster = #{id => brick_cluster, start => {brick_cluster, start_link, []}, restart => permanent, type => worker},
 	Event = #{id => brick_event, start => {brick_event, start_link, []}, restart => permanent, type => worker},
+	Gossip = #{id => brick_gossip, start => {brick_gossip, start_link, []}, restart => permanent, type => worker},
 	
 	Optional = optional(),
 
 	SupFlags = #{strategy => one_for_one, intensity => 2, period => 10},
-	Procs = [State, Clock, Cluster, Event] ++ Optional,
+	Procs = [State, Clock, Cluster, Event, Gossip] ++ Optional,
 	{ok, {SupFlags, Procs}}.
 
 %% ====================================================================
